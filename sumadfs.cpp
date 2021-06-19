07304e74c2b80769cd260856bc9344b76179ba0c
luisolis20 <86112119+luisolis20@users.noreply.github.com>
2021-06-19 17:31:05 -0500

    Update sumadfs.cpp
    Estamos haaciendo una parte para la implementacion de la visualizacion de los elementos de una matriz :)
    Utilizaremos 2 for

--- sumadfs.cpp
+++ sumadfs.cpp
@@ -6,7 +6,27 @@ int c=0;
 float matriz1[10]={2.3,4.5,6.5,0.5,6.1,5.5,1.2,2.4,6.5,9.1};
 float matriz2[10]={2.3,4.5,6.5,0.5,6.1,5.5,1.2,2.4,6.5,9.1};
 float suma[10],resta[10],producto[10],division[10];
-
+//Esta es la parte de cod de Montaño
+  for(int c=0;c<10;c++){
+    
+  }
+  
+  
+  
+  
+  //En esta parte de cod de Giler 
+  
+  
+  
+  //En esta parte de cod de ..........
+  
+  
+  
+  
+  //En esta parte de cod de ...........
+  
+  
+  
 
 
 
