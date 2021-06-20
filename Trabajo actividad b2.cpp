4db16650fa7250b35a24ce2ef6a6e17ef2604df4
luisolis20 <86112119+luisolis20@users.noreply.github.com>
2021-06-20 15:33:02 -0500

    Update Trabajo actividad b2.cpp

--- Trabajo actividad b2.cpp
+++ Trabajo actividad b2.cpp
@@ -8,7 +8,7 @@ float matriz2[10]={2.3,4.5,6.5,0.5,6.1,5.5,1.2,2.4,6.5,9.1};
 float suma[10],resta[10],producto[10],division[10];
 //Michael Becerra
 cout<<"\n";
-cout<<"Aquí se muestra la matriz 1 y 2"<<endl;
+cout<<"AquÃ­ se muestra la matriz 1 y 2"<<endl;
 cout<<"**********************************"<<endl;
 for(int c=0;c<10;c++){
 		cout<<matriz1[c];
@@ -23,7 +23,7 @@ for(int c=0;c<10;c++){
 cout<<endl;
 //	Anni Giler
 cout<<"\n";
-cout<<"Aquí se muestra la suma de la matriz 1 y 2"<<endl;
+cout<<"AquÃ­ se muestra la suma de la matriz 1 y 2"<<endl;
 cout<<"**********************************"<<endl;
 for(int c=0;c<10;c++){
 	cout<<matriz1[c]+matriz2[c];
@@ -32,7 +32,7 @@ for(int c=0;c<10;c++){
 cout<<endl;
 // Joel Eras 
 cout<<"\n";
-cout<<"Aquí se muestra la resta de la matriz 1 y 2"<<endl;
+cout<<"AquÃ­ se muestra la resta de la matriz 1 y 2"<<endl;
 cout<<"**********************************"<<endl;
 for(int c=0;c<10;c++){
 	cout<<matriz1[c]-matriz2[c];
@@ -41,16 +41,16 @@ for(int c=0;c<10;c++){
 cout<<endl;
 // Jose Baez  
 cout<<"\n";
-cout<<"Aquí se muestra la multiplicacion de la matriz 1 y 2"<<endl;
+cout<<"AquÃ­ se muestra la multiplicacion de la matriz 1 y 2"<<endl;
 cout<<"**********************************"<<endl;
 for(int c=0;c<10;c++){
 	cout<<matriz1[c]*matriz2[c];
 	cout<<" - ";
 }
 cout<<endl;
-// Montaño Luis
+//  Luis MontaÃ±o
 cout<<"\n";
-cout<<"Aquí se muestra la  division de la matriz 1 y 2"<<endl;
+cout<<"AquÃ­ se muestra la  division de la matriz 1 y 2"<<endl;
 cout<<"**********************************"<<endl;
 for(int c=0;c<10;c++){
 	cout<<matriz1[c]/matriz2[c];
