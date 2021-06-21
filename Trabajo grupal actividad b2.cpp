eb3ddf3ba4d2f0b906a75d8b780cfea92be60976
Michaelbece <86241640+Michaelbece@users.noreply.github.com>
2021-06-21 08:36:43 -0500

    Update Trabajo grupal actividad b2.cpp
    Yo Hice la parte de Mostrar los elementos de una matriz
    Soy Michael Becerra

--- Trabajo grupal actividad b2.cpp
+++ Trabajo grupal actividad b2.cpp
@@ -6,7 +6,20 @@ int c=0;
 float matriz1[10]={2.3,4.5,6.5,0.5,6.1,5.5,1.2,2.4,6.5,9.1};
 float matriz2[10]={2.3,4.5,6.5,0.5,6.1,5.5,1.2,2.4,6.5,9.1};
 float suma[10],resta[10],producto[10],division[10];
-
+//Michael Becerra
+cout<<"\n";
+cout<<"Aqui se muestra la matriz 1 y la matriz 2"<<endl;
+cout<<"**********************************"<<endl;
+for(int c=0;c<10;c++){
+		cout<<matriz1[c];
+		cout<<" - ";
+}
+cout<<endl;
+for(int c=0;c<10;c++){
+		cout<<matriz2[c];
+		cout<<" - ";
+		
+}
 
 
 
